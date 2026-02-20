e2a94ce793f3eb1eca89f68c2388d15376493f8f
Richard Bricio <richard.bricio@gmail.com>
2026-02-20 13:53:47 -0300

    Update route.ts

--- api/upload/route.ts
+++ api/upload/route.ts
@@ -12,7 +12,9 @@ export async function POST(req: Request) {
 
   const blob = await put(file.name, file, {
     access: "public",
+    addRandomSuffix: true,
   });
-
+  
   return Response.json(blob);
+
 }
