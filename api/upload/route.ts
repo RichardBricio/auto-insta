cbb10f0790e3c31c8ed2bb926921ffd2d1782175
Richard Bricio <richard.bricio@gmail.com>
2026-02-20 13:44:38 -0300

    Add files via upload

---
+++ api/upload/route.ts
@@ -0,0 +1,18 @@
+import { put } from "@vercel/blob";
+
+export const runtime = "nodejs";
+
+export async function POST(req: Request) {
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
+
+  if (!file) {
+    return new Response("No file", { status: 400 });
+  }
+
+  const blob = await put(file.name, file, {
+    access: "public",
+  });
+
+  return Response.json(blob);
+}
